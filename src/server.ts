c1f87110d931d8c1806d087befc9dfbbfd79e55c
johnsjohns <o-anao@hotmail.com>
2021-04-24 11:31:20 -0300

    Finalizando aula 2: Migrations, Rotas e Entitities

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
-import express, { request } from "express";
+import express from "express";
+import "./database";
+import { routes } from "./routes";
+
 
 const app = express();
 
-app.get('/', (resquest, response) =>{
-    return response.json({
-        message: "Ola NLW 005",
-    });
-});
-
-app.post('/', (resquest, response) =>{
-    return response.json({
-        message: "Usuario salvo com sucesso",
-    })
-})
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333, () => console.log('Server is running on port 3333'));
